c41f3058f8bff2705b25b32a52c220a75ff6c252
Екатерина Савинкова <katy4.savinkova2004@yanedx.ru>
2023-03-20 22:16:50 +0300

    add arguments to gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,9 @@ const program = new Command();
 
 program
     .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0');
+    .version('1.0.0')
+    .arguments('<filepath1> <filepath2>')
+    .option('-f, --format <type>', 'output format');
 
 
 program.parse();
