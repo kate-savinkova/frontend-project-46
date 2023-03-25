9ee28564d200e5866aa37061446f5e824e75fb43
Екатерина Савинкова <katy4.savinkova2004@yanedx.ru>
2023-03-25 21:51:57 +0300

    make yml files comparing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import { readFileSync } from "fs";
 import _ from "lodash";
+import { fileURLToPath } from "url";
+import path from "path";
+import yaml from "js-yaml";
 
-const innerFunc = (obj1, obj2, deep) => {
+const convertFile = (file) => {
+  const ext = path.extname(file);
+  if (ext === ".json") {
+    return JSON.parse(readFileSync(file, "utf-8"));
+  }
+  if (ext === ".yml") {
+    return yaml.load(readFileSync(file, "utf8"));
+  }
+  return null;
+};
+
+const getFixturePath = (filename) => {
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  return path.join(__dirname, "..", "__fixtures__", filename);
+};
+
+const compareFiles = (obj1, obj2, deep) => {
   const replace = "  ";
   const keys1 = _.keys(obj1);
   const keys2 = _.keys(obj2);
@@ -20,7 +39,7 @@ const innerFunc = (obj1, obj2, deep) => {
     if (_.has(obj1, key)) {
       if (_.has(obj2, key)) {
         if (typeof obj1[key] === "object" && typeof obj2[key] === "object") {
-          const inner = innerFunc(obj1[key], obj2[key], deep + 1);
+          const inner = compareFiles(obj1[key], obj2[key], deep + 1);
           res.push(`${replace.repeat(deep + 1)}  ${key}:\n${inner}`);
         } else if (obj1[key] === obj2[key]) {
           res.push(`${replace.repeat(deep + 1)}  ${key}: ${obj1[key]}`);
@@ -29,13 +48,13 @@ const innerFunc = (obj1, obj2, deep) => {
           res.push(`${replace.repeat(deep + 1)}+ ${key}: ${obj2[key]}`);
         }
       } else if (typeof obj1[key] === "object") {
-        const inner = innerFunc(obj1[key], {}, deep + 1);
+        const inner = compareFiles(obj1[key], {}, deep + 1);
         res.push(`${replace.repeat(deep + 1)}- ${key}:\n${inner}`);
       } else {
         res.push(`${replace.repeat(deep + 1)}- ${key}: ${obj1[key]}`);
       }
     } else if (typeof obj2[key] === "object") {
-      const inner = innerFunc(obj2[key], {}, deep + 1);
+      const inner = compareFiles(obj2[key], {}, deep + 1);
       res.push(`${replace.repeat(deep + 1)}+ ${key}:\n${inner}`);
     } else {
       res.push(`${replace.repeat(deep + 1)}+ ${key}: ${obj2[key]}`);
@@ -46,12 +65,4 @@ const innerFunc = (obj1, obj2, deep) => {
   return res.join("\n");
 };
 
-const genDiff = (filepath1, filepath2) => {
-  const data1 = JSON.parse(readFileSync(filepath1, "utf-8"));
-  const data2 = JSON.parse(readFileSync(filepath2, "utf-8"));
-
-  const res = innerFunc(data1, data2, 0);
-  return res;
-};
-
-export default genDiff;
+export { getFixturePath, convertFile, compareFiles };
