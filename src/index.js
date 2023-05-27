357811f72d7609772307eee926a67bfaa9ba79b7
Екатерина Савинкова <katy4.savinkova2004@yanedx.ru>
2023-05-27 13:53:17 +0300

    fix formats

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import buildTree from "./buildTree.js";
 import formatData from "./formatters/index.js";
 import parse from "./parsers.js";
 
-const genDiff = (filePath1, filePath2, format) => {
+const genDiff = (filePath1, filePath2, format = 'stylish') => {
   const data1 = parse(filePath1);
   const data2 = parse(filePath2);
   const diffInfo = buildTree(data1, data2);
