993110f78cc3fcd64056c4751ceed9b16be8bd14
Екатерина Савинкова <katy4.savinkova2004@yanedx.ru>
2023-05-27 15:52:40 +0300

    fix problems with linter

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,15 +6,19 @@ const inputFormats = ['json', 'yml'];
 
 const getPath = (filename, format) => path.resolve('.', '__fixtures__', `${filename}.${format}`);
 
-var output;
+const output = {
+  stylish: readFileSync(getPath('result-stylish-output', 'txt'), 'utf-8'),
+  plain: readFileSync(getPath('result-plain-output', 'txt'), 'utf-8'),
+  json: readFileSync(getPath('result-json-output', 'txt'), 'utf-8'),
+};
 
-beforeAll(() => {
+/*beforeAll(() => {
   output = {
     stylish: readFileSync(getPath('result-stylish-output', 'txt'), 'utf-8'),
     plain: readFileSync(getPath('result-plain-output', 'txt'), 'utf-8'),
     json: readFileSync(getPath('result-json-output', 'txt'), 'utf-8'),
   };
-});
+});*/
 
 describe.each(inputFormats)('gendiff for %p files', (format) => {
   const f1 = `f1.${format}`;
